338aa107b284709c1705f0d5874e1443ce09467a
DimaZhukov <zhukovdima@list.ru>
2018-05-12 16:27:15 +0600

    Adding LocalDate parser

--- src/main/java/com/lunchvoting/util/DateTimeUtil.java
+++ src/main/java/com/lunchvoting/util/DateTimeUtil.java
@@ -1,5 +1,7 @@
 package com.lunchvoting.util;
 
+import org.springframework.util.StringUtils;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,4 +15,12 @@ public class DateTimeUtil {
 
     public static final LocalDateTime CURRENT_DAY_END_DATE_TIME =
             LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59));
+
+    public static LocalDate parseLocalDate(String str) {
+        return StringUtils.isEmpty(str) ? null : LocalDate.parse(str);
+    }
+
+    public static LocalTime parseLocalTime(String str) {
+        return StringUtils.isEmpty(str) ? null : LocalTime.parse(str);
+    }
 }
